e373ddde7de87bedc75adedc735dbe7943a01221
ccwrc <ccwrcltd@gmail.com>
2018-06-11 11:46:49 +0200

    first validator in Quiz entity

--- src/Entity/Quiz.php
+++ src/Entity/Quiz.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\QuizRepository")
@@ -24,11 +25,24 @@ class Quiz
     private $questions;
 
     /**
+     * @Assert\Type("string")
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 220
+     * )
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $title;
 
     /**
+     * @Assert\Range(
+     *      min = 1,
+     *      max = 100,
+     *      minMessage = "Min value is {{ limit }}",
+     *      maxMessage = "Max value is {{ limit }}"
+     * )
+     *
      * @ORM\Column(type="integer")
      */
     private $percentageCorrectnessToWin;
